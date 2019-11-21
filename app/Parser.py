ba5c1b2fb58855ded7f19192547f4c121ccb2106
Geoffrey Remacle <geoffreyremacle@outlook.fr>
2019-11-21 21:24:16 +0100

    Parser parses correctly and all tests are green

--- app/Parser.py
+++ app/Parser.py
@@ -1,11 +1,84 @@
+import re
+import json
 class Parser():
     def __init__(self):
         self.question_to_analyse = None # this value will get a value when "get_question" method will be called
+        self.parsed_question = None
+        self.ACCENTS_DICT = {
+                             ("à" ,"â","ä") : "a",
+                             ("ù", "ü", "û") : "u",
+                             ("é", "è", "ë","ê"): "e",
+                             ("ô", "ö") : "o",
+                             ("î", "ï") : "i",
+                             ("ç") : "c",
+                             (",", ":", ";", "\"", "_", "-") : "",
+                             ("'") : " "
+                             }
+        self.part_of_regex = "([a-z0-9]*[ ])([ a-z0-9-]*)([.?!])"
+        self.keywords_list = ["ou se trouv", "ou se situe", "se localis", "l adresse", "le lieu", "l endroit", "le monument"]
+        self.stopwords_list = None
+    
+    def import_stopwords(self):
+        with open("stopwords.json") as f:
+            self.stopwords_list = json.load(f)
+
 
     def get_question(self):
         """
         ask to user a question
         """
         question = input("Posez votre question à grandPy: ")
-        self.question_to_analyse = question
+        self.question_to_analyse = question + "."
+
+    def lower_user_question(self):
+        return self.question_to_analyse.lower()
+        
+    def eliminate_accents_of_question(self, question):
+        question = list(question)
+        for index, letter in enumerate(question):
+            for key in self.ACCENTS_DICT.keys():
+                if letter in key:
+                    question[index] = self.ACCENTS_DICT[key]
+        question_without_accents = "".join(question)
+        return question_without_accents
+    
+    def find_place_in_question(self, question):
+        for keyword in self.keywords_list:
+            regex = keyword + self.part_of_regex
+            re_answer = re.search(regex, question)
+            if re_answer:
+                return (True, re_answer.group(2))
+        return (False, "")
+
+    def remove_stopwords(self, sentence):
+        words_list = sentence.split(" ")
+        for index, word in enumerate(words_list):
+            for stopword in self.stopwords_list:
+                if word == stopword:
+                    words_list[index] = ""
+        words_list = [word for word in words_list if word != ""]
+        return " ".join(words_list)
+
+    
+    def parse(self):
+        question_lowed = self.lower_user_question()
+        question_no_accent = self.eliminate_accents_of_question(question_lowed)
+        interesting_part_of_question = self.find_place_in_question(question_no_accent)
+        if interesting_part_of_question[0]== True:
+            interesting_part_without_stopwords = self.remove_stopwords(interesting_part_of_question[1])
+        elif interesting_part_of_question[0] == False:
+            return "Désolé mais je ne comprends pas la question"
+        self.parsed_question = interesting_part_without_stopwords
+    
+if __name__ == "__main__":
+    parser = Parser()
+    parser.import_stopwords()
+    parser.get_question()
+    parser.parse()
+
+
+
+
+
+
     
--- tests/test_Parser.py
+++ tests/test_Parser.py
@@ -1,10 +1,16 @@
-import app.Parser
+import app.Parser, pytest
 
 def test_class_Parser_exists():
     assert hasattr(app.Parser, "Parser")
 
-def test_get_question_method_add_value_to_question_to_analyse_attribute(monkeypatch):
-    user_question = "Bonjour GrandPy, j'espère que tu vas bien, sais-tu où se trouve la tour eiffel?"
+@pytest.mark.parametrize("user_question", [
+    "Bonjour GrandPy, j'espère que tu vas bien, sais-tu où se trouve la tour eiffel?",
+    "Salut, connais-tu la grand Place à Bruxelles?",
+    "Hey, j'aimerais bien avoir des infos sur le théatre de Gant."
+])
+
+def test_get_question_method_add_value_to_question_to_analyse_attribute(user_question, monkeypatch):
+
     def mock_input(string=None):
         return user_question
     
@@ -12,5 +18,85 @@ def test_get_question_method_add_value_to_question_to_analyse_attribute(monkeypa
     monkeypatch.setattr("builtins.input", mock_input)
     parser.get_question()
 
-    assert parser.question_to_analyse == user_question
+    assert parser.question_to_analyse == user_question + "."
 
+@pytest.mark.parametrize("user_question", [
+    "Bonjour GrandPy, j'espère que tu vas bien ! Connais-tu le lieu suivant : Place de la Bastille ? Bien à toi.",
+    "BoNjoUr granDpy, j'Espère que tU vAs bien ! connais-tu LE lieu suivant : place de la BasTille ? bien à toi.",
+    "BONJOUR GRANDPY, J'ESPèRE QUE TU VAS BIEN ! CONNAIS-TU LE LIEU SUIVANT : PLACE DE LA BASTILLE ? BIEN à TOI."    
+])
+def test_lower_question_is_ok(user_question):
+    """
+    à demander: savoir si il faut mocker la fonction lower ou pas
+    """
+    parser = app.Parser.Parser()
+    parser.question_to_analyse = user_question
+    question_lowed = parser.lower_user_question()
+    assert  question_lowed == "bonjour grandpy, j'espère que tu vas bien ! connais-tu le lieu suivant : place de la bastille ? bien à toi."
+
+def test_method_eliminate_accents_of_questions_does_the_job():
+    parser = app.Parser.Parser()
+    question = "été soleil ça où été être cela île àâä ùüû ôö îï,,:;\""
+    assert parser.eliminate_accents_of_question(question) == "ete soleil ca ou ete etre cela ile aaa uuu oo ii"
+
+def test_stopwords_list_attribute_is_assigned_well_to_attribute(monkeypatch):
+
+    stopwords = ["a", "un", "une", "as", "des", "le", "la", "d", "et", "de", "s","il","te","plait"]
+
+    class MockOpen():
+        def __init__(self, directory):
+            pass
+        def __enter__(self):
+            pass
+        def __exit__(self, type, value, traceback):
+            pass
+    def mock_json_load(file):
+        return stopwords
+    parser = app.Parser.Parser()
+    monkeypatch.setattr("builtins.open", MockOpen)
+    monkeypatch.setattr("json.load", mock_json_load)
+    parser.import_stopwords()
+    assert parser.stopwords_list == stopwords
+
+@pytest.mark.parametrize("question", [
+    ("""salut grandpy! Comment s est passe ta soiree avec grandma hier soir? au fait, pendant que j y pense,
+    pourrais tu m indiquer ou se trouve le musee d art et d histoire de fribourg s il te plait?""", "le musee d art et d histoire de fribourg s il te plait"),
+    ("""bonsoir grandpy, j espere que tu as passe une belle semaine. est ce que tu pourrais m'indiquer
+       l adresse de la tour eiffel? merci d avance et salutations a mamie.
+    """, "de la tour eiffel")
+])
+
+def test_find_place_in_questions_find_good__part_of_sentence(question):
+    parser = app.Parser.Parser()
+    good_part_of_question = parser.find_place_in_question(question[0])
+    print(good_part_of_question)
+    assert good_part_of_question[1] == question[1]
+    
+
+def test_remove_stopwords_removes_well_given_stopwords_of_a_sentence():
+    parser = app.Parser.Parser()
+    parser.stopwords_list = ["à", "ah", "bon", "salut", "ça", "va", "et", "de", "en"]
+    parsed_sentence = parser.remove_stopwords("salut GrandPy, comment ça va aujourd'hui ? ah j'ai oublié de te dire que je vais aller en France.")
+    assert parsed_sentence == "GrandPy, comment aujourd'hui ? j'ai oublié te dire que je vais aller France."
+
+@pytest.mark.parametrize("question", [
+    ("""Salut grandpy! Comment s'est passé ta soirée avec Grandma hier soir? Au fait, pendant que j'y pense,
+    pourrais-tu m'indiquer où se trouve le musée d'art et d'histoire de Fribourg, s'il te plaît?""", "musee art histoire fribourg"),
+    ("""
+    Bonsoir Grandpy, j'espère que tu as passé une belle semaine. Est-ce que tu pourrais m'indiquer 
+    l'adresse de la tour eiffel? Merci d'avance et salutations à Mamie.""", "tour eiffel")
+])
+
+def test__method_parse_assign_expected_value_to_parsed_question_attribute(question, monkeypatch):
+    def mock_import_stopwords(self):
+        self.stopwords_list = ["a", "un", "une", "as", "des", "le", "la", "d", "et", "de", "s","il","te","plait"]
+    monkeypatch.setattr("app.Parser.Parser.import_stopwords", mock_import_stopwords)
+    parser = app.Parser.Parser()
+    parser.import_stopwords()
+    parser.question_to_analyse = question[0]
+    parser.parse()
+    print(parser.parsed_question)
+    assert parser.parsed_question == question[1]
+    
+
+    
