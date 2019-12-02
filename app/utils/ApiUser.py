acd7cabe8f882c4d3d5a9a9af787f6e5c453e5e4
Geoffrey Remacle <geoffreyremacle@outlook.fr>
2019-12-02 09:59:01 +0100

    finalisation of test on apiuser class

--- app/utils/ApiUser.py
+++ app/utils/ApiUser.py
@@ -1,2 +1,87 @@
+import requests
 class ApiUser():
-    pass
+    def __init__(self):
+        self.HTTP_BASE_REQUEST_API_HERE_GEOCOORDS = "https://places.cit.api.here.com/places/v1/discover/search?"
+        self.HTTP_BASE_REQUEST_API_HERE_IMAGE = "https://image.maps.api.here.com/mia/1.6/mapview?"
+        self.HTTP_BASE_REQUEST_API_WIKIPEDIA = "https://fr.wikipedia.org/w/api.php?"
+        self.place_to_find = None
+        self.app_id, self.app_code = "hAwtcHZDT2E4LNBNo3Ju", "5R3EaOfVBeF8BB-BCnBvvg"
+        self.wiki_id, self.wiki_infos = None, None
+        self.lon, self.lat = None, None
+        self.binary_image = None
+        
+    def set_place_to_find(self, place):
+        self.place_to_find = place
+    
+    def clear_attributes(self):
+        self.place_to_find = None
+        self.wiki_id, self.wiki_infos = None, None
+        self.lon, self.lat = None, None
+        self.binary_image = None
+    
+    def construct_http_request(self, find_geocoords=False, find_map=False, find_wiki_infos=False, get_wiki_id=False):
+        if find_geocoords == True:
+            parameters_http_request = "app_id={0}&app_code={1}&q={2}&at=0,0".format(self.app_id, self.app_code, self.place_to_find)
+            return self.HTTP_BASE_REQUEST_API_HERE_GEOCOORDS + parameters_http_request
+        elif find_map == True:
+            parameters_http_request = "app_id={0}&app_code={1}&c={2},{3}&z=14".format(self.app_id, self.app_code, self.lon, self.lat)
+            return self.HTTP_BASE_REQUEST_API_HERE_IMAGE + parameters_http_request
+        elif find_wiki_infos == True:
+            parameters_http_request = "action=query&pageids={0}&prop=extracts&explaintext=true&exsectionformat=wiki&format=json".format(self.wiki_id)
+            return self.HTTP_BASE_REQUEST_API_WIKIPEDIA + parameters_http_request
+        elif get_wiki_id == True: 
+            parameters_http_request = "action=query&list=geosearch&gscoord={0}|{1}&gsradius=10000&gslimit=10&format=json".format(self.lon, self.lat)
+            return self.HTTP_BASE_REQUEST_API_WIKIPEDIA + parameters_http_request
+
+    
+    def send_http_request(self, param_dict):
+        request = self.construct_http_request(**param_dict)
+        try:
+            response = requests.get(request)
+        except:
+            response = {}
+        
+        
+        return response
+    
+    def trait_http_request(self, response, param_dict):
+        if param_dict["find_map"] == False:
+            json_response = response.json()
+        if param_dict["find_geocoords"] == True:
+            self.lon, self.lat = json_response["results"]["items"][0]["position"]
+        elif param_dict["find_map"] == True:
+            self.binary_image = response.content
+
+        elif param_dict["get_wiki_id"] == True:
+            self.wiki_id = json_response["query"]["geosearch"][0]["pageid"]
+
+        elif param_dict["find_wiki_infos"] == True:
+            self.wiki_infos = json_response["query"]["pages"][str(self.wiki_id)]
+    
+    def find_all_informations(self):
+        modif_list = [(("find_geocoords", False), ("find_map", True)), (("find_map", False), ("get_wiki_id", True)),
+                      (("get_wiki_id", False), ("find_wiki_infos", True))
+                     ]
+        index = -1
+        param_dict = {"find_geocoords": True,
+                          "find_map" : False,
+                          "get_wiki_id" : False,
+                          "find_wiki_infos" : False}
+        while index < 4:
+            response = self.send_http_request(param_dict)
+            self.trait_http_request(response, param_dict)
+            if index >= 0 and index < 3:
+                param_dict[modif_list[index][0][0]] = modif_list[index][0][1]
+                param_dict[modif_list[index][1][0]] = modif_list[index][1][1]
+            index += 1
+    
+    def give_informations(self):
+        return [self.wiki_infos, self.binary_image]
+            
+
+if __name__ == "__main__":
+    api = ApiUser()
+    api.set_place_to_find("la chapelle sixtinne")
+    api.find_all_informations()
+    print(api.give_informations())
+
--- tests/test_ApiUser.py
+++ tests/test_ApiUser.py
@@ -1,3 +1,72 @@
 import app.utils.ApiUser as ApiUser
+import pytest
 def test_class_ApiUser_exists():
     assert hasattr(ApiUser, "ApiUser")
+
+def test_set_place_to_find_gives_value_to_attribute_place_to_find():
+    api_user = ApiUser.ApiUser()
+    api_user.set_place_to_find("chapelle Sixtine")
+    assert api_user.place_to_find == "chapelle Sixtine"
+
+def test_clear_attribute_method_clears_every_attributes():
+    api_user = ApiUser.ApiUser()
+    api_user.place_to_find = "test"
+    api_user.wiki_id, api_user.wiki_infos = "test", "test"
+    api_user.lon, api_user.lat = "test", "test"
+    api_user.binary_image = "test"
+    api_user.clear_attributes()
+    attributes_list = [api_user.place_to_find, api_user.wiki_id, api_user.wiki_infos,
+                       api_user.lon, api_user.lat, api_user.binary_image]
+    for attr in attributes_list:
+        assert attr == None
+
+def test_construct_http_request_for_geocoords():
+    api_user = ApiUser.ApiUser()
+    api_user.set_place_to_find("example_test")
+    api_user.app_id, api_user.app_code = "user1", "abc1234"
+    http_request = api_user.construct_http_request(find_geocoords=True)
+    assert http_request == api_user.HTTP_BASE_REQUEST_API_HERE_GEOCOORDS + "app_id=user1&app_code=abc1234&q=example_test&at=0,0"
+
+def test_construct_http_request_for_image():
+    api_user = ApiUser.ApiUser()
+    api_user.lon, api_user.lat = 2,4
+    api_user.app_id, api_user.app_code = "user1", "abc1234"
+    http_request = api_user.construct_http_request(find_map=True)
+    assert http_request == api_user.HTTP_BASE_REQUEST_API_HERE_IMAGE + "app_id=user1&app_code=abc1234&c=2,4&z=14"
+
+def test_construct_http_request_for_wiki_to_find_id():
+    api_user = ApiUser.ApiUser()
+    api_user.lon, api_user.lat = 2,4
+    api_user.app_id, api_user.app_code = "user1", "abc1234"
+    http_request = api_user.construct_http_request(get_wiki_id=True)
+    assert http_request == api_user.HTTP_BASE_REQUEST_API_WIKIPEDIA + "action=query&list=geosearch&gscoord=2|4&gsradius=10000&gslimit=10&format=json"
+
+def test_construct_http_request_for_wiki_to_find_infos():
+    api_user = ApiUser.ApiUser()
+    api_user.wiki_id = 45293
+    api_user.app_id, api_user.app_code = "user1", "abc1234"
+    http_request = api_user.construct_http_request(find_wiki_infos=True)
+    assert http_request == api_user.HTTP_BASE_REQUEST_API_WIKIPEDIA + "action=query&pageids=45293&prop=extracts&explaintext=true&exsectionformat=wiki&format=json"
+
+def test_send_http_request_return_response_with_mock_request_get(monkeypatch):
+    class MockResponse():
+        pass
+
+    def mock_construct_http_request(object, find_geocoords=False, find_map=False, find_wiki_infos=False, get_wiki_id=False):
+        return "https://api/test.com"
+
+    def mock_requests_get(url):
+        return MockResponse()
+    
+    monkeypatch.setattr("app.utils.ApiUser.ApiUser.construct_http_request", mock_construct_http_request)
+    monkeypatch.setattr("requests.get", mock_requests_get )
+    api_user = ApiUser.ApiUser()
+    response_object = api_user.send_http_request({"find_geocoords" : False, "find_map" : True, "find_wiki_infos" : False, "get_wiki_id" : False})
+    assert type(response_object) == MockResponse
+
+def test_trait_http_request():
+    pass
+
+
+    
+    
